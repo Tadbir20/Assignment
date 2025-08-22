8237be8674c717ec0bd710c903963bd93154a3b6
Tadbir20 <tadbirtareqg2@gmail.com>
2025-08-22 22:25:40 +0600

    updated invoice

--- Assingment 2/Invoice/InvoiceBuilder.php
+++ Assingment 2/Invoice/InvoiceBuilder.php
@@ -1,23 +1,51 @@
 <?php 
-//create Invoice - My First Invoice.txt
-/**
-My First Invoice
-Item 1 - 100
-Item 2 - 200
------------------
-Total = 300
- */
-class InvoiceBuilder{
-    function addTitle($tile){}
-    function addItem($item, $price){}
+class Invoice{
 
-    function addTotal(){}
-    function createInvoice(){}
+    private $title;
+    private $items = array();
+    private $total = 0;
+
+    function addTitle($title){
+        $this->title = $title;
+    }
+    function addItem($item, $price){
+        $this->items[] = array("name" => $item, "price" => $price);
+    }
+
+    function addTotal(){
+        // for ($i = 0; $i < count($this->items); $i++) {
+        //     $this->total += $this->items[$i]['price'];
+        // }
+        foreach ($this->items as $item) {
+            $this->total += $item['price'];
+        }
+    }
+    function createInvoice(){
+        $filePath = "Assingment 2/Invoice/InvoiceBuilder.txt";
+        $invoice = $this->title . "\n \n";
+        foreach ($this->items as $item) {
+            $invoice .= $item['name'] . " - " . $item['price'] . "\n";
+        }
+        $invoice .= "-----------------\n";
+        $invoice .= "Total = " . $this->total . "\n";
+        $file = fopen($filePath, "a");
+        fwrite($file, $invoice);
+        fclose($file);
+    }
 }
 
-$inv = new InvoiceBuilder();
-$inv->addTitle("My First Invoice");
-$inv->addItem("Item 1", 100);
-$inv->addItem("Item 2", 200);
-$inv->addTotal();
-$inv->createInvoice();
+$inv1 = new Invoice();
+$inv1->addTitle("My First Invoice");
+$inv1->addItem("Item 1", 100);
+$inv1->addItem("Item 2", 200);
+$inv1->addItem("Item 3", 300);
+$inv1->addTotal();
+$inv1->createInvoice();
+
+$inv2 = new Invoice();
+$inv2->addTitle("My Second Invoice");
+$inv2->addItem("Item A", 150);
+$inv2->addItem("Item B", 250);
+$inv2->addItem("Item C", 350);
+$inv2->addTotal();
+$inv2->createInvoice();
